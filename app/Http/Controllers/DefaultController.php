b3cb6ef1a62841fdb10b6a29fe6da3c68568d8c6
kme222yh <49933526+kme222yh@users.noreply.github.com>
2021-09-03 20:26:06 +0900

    日記一覧sql修正

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -9,12 +9,14 @@ use Carbon\Carbon;
 class DefaultController extends Controller
 {
     public function index(){
-        $diaries = Diary::where('last_displayed', '<=', Carbon::now()->addSecond(-config('diary.interval')))->
-                            orWhereNull('last_displayed')->
-                            where('published', true)->
-                            inRandomOrder()->
-                            take((int)config('diary.display_ata_time'))->
-                            get();
+        $diaries = Diary::where('published', true)
+            ->where(function($query){
+                    $query->Where('last_displayed', '<=', Carbon::now()
+                    ->addSecond(-config('diary.interval')))->orWhereNull('last_displayed');
+            })
+            ->inRandomOrder()
+            ->take((int)config('diary.display_ata_time'))
+            ->get();
         $t = Carbon::now();
         foreach ($diaries as $diary) {
             $diary->last_displayed = $t;
