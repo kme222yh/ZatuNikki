913a592e59f35cc398fed3186c460fbb8bbdb3c5
kme222yh <49933526+kme222yh@users.noreply.github.com>
2021-09-21 21:15:37 +0900

    過去の日記表示

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -8,7 +8,9 @@ use Carbon\Carbon;
 
 class DefaultController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+
+        // randomly selected diaries
         $diaries = Diary::where('published', true)
             ->where(function($query){
                     $query->Where('last_displayed', '<=', Carbon::now()
@@ -17,13 +19,26 @@ class DefaultController extends Controller
             ->inRandomOrder()
             ->take((int)config('diary.display_ata_time'))
             ->get();
+
+        // recommended diaries for the logged-in user according to forgetting curve
+        $fc_dates = [1, 4, 7, 11, 15, 20, 30];
+        $recomendations = [];
+        if($request->user()){
+            foreach ($fc_dates as $fc_date) {
+                $diary = $request->user()->diaries()->whereDate('date', Carbon::today()->addDay(-$fc_date))->first();
+                if($diary)  $recomendations[] = $diary;
+            }
+        }
+
         $t = Carbon::now();
         foreach ($diaries as $diary) {
             $diary->last_displayed = $t;
             $diary->save();
         }
+
         return view('top.top', [
             "diaries" => $diaries,
+            "recomendations" => $recomendations,
         ]);
     }
 }
--- resources/views/top/diaryList.blade.php
+++ resources/views/top/diaryList.blade.php
@@ -1,6 +1,19 @@
 <div class="diarylist">
+    @if(count($recomendations)>0)
+    <p class="diarylist-recommendtext"><i class="fas fa-undo-alt"></i> 今日はこの日記を振り返ってみましょう</p>
     <div class="diarylist-body">
-        @if(count($diaries)<=0)
+        @foreach ($recomendations as $diary)
+        <article class="diarylist-item">
+            <a href="{{ route('diary.show', ['diary' => $diary->id]) }}"></a>
+            <h2 class="diarylist-item-title">{!! e($diary->getTitle()) !!}</h2>
+            <p class="diarylist-item-date">{{ $diary->date->format('Y/m/d') }}</p>
+            <p class="diarylist-item-contents">{!! nl2br(e($diary->getContentOnly5Rows(true))) !!}</p>
+        </article>
+        @endforeach
+    </div>
+    @endif
+    <div class="diarylist-body">
+        @if(count($diaries)<=0 && count($recomendations)<=0)
         <div class="diarylist-null">
             <h1 class="diarylist-null-message">日記がありません…</h1>
             <img class="diarylist-null-image" src="{{ asset("images/logo/close_gray.png") }}" alt="">
