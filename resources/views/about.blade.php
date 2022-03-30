0007b00f5af152fd25fdd97918cdd5981f9f4217
kme222yh <49933526+kme222yh@users.noreply.github.com>
2022-03-30 12:24:15 +0900

    change text

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -34,20 +34,13 @@
             <p class="about-content">
                 ここは日記を書く場所です <br>
                 いつでもどこでも、すぐに書くことができます <br>
-                「日記帳を開くのが面倒だから…」「忙しくて家に帰ってから書く時間がない…」といった理由で日記を書くのをためらっているのなら、ここで書いてみてはどうですか？ <br>
-                <br>
-                ここで書く日記はあなただけのものですが、知らない誰かにみせることができます <br>
-                もちろん誰がかいたのかはわかりません <br>
-                たくさんの人が読めば、その感想が巡り巡ってあなたに届くかもしれませんね <br>
-
-            </p>
-            <img class="about-img" src="{{ asset("images/logo/open_gray.png") }}" alt="">
-            <p class="about-content">
-                ここはあなただけの新しい日記帳です <br>
-                その日の感想、明日やること、お悩みごと <br>
-                日記に書く内容はなんでもかまいません <br>
                 さあ、日記をかきましょう！
             </p>
+            <img class="about-img" src="{{ asset("images/logo/open_gray.png") }}" alt="">
+
+            @if(Auth::check())
+            <button class="authform-button" type="submit" name="button">にっきをかく</button>
+            @endif
         </div>
     </div>
 
