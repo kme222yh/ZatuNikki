88cd36b9df5f971c635c3cc80487af1387b0dbf8
kme222yh <49933526+kme222yh@users.noreply.github.com>
2022-06-07 14:58:56 +0900

    ざつにっき側で本表示

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -5,20 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Models\Diary;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DefaultController extends Controller
 {
     public function index(Request $request){
 
         // randomly selected diaries
-        $diaries = Diary::where('published', true)
-            ->where(function($query){
-                    $query->Where('last_displayed', '<=', Carbon::now()
-                    ->addSecond(-config('diary.interval')))->orWhereNull('last_displayed');
-            })
-            ->inRandomOrder()
-            ->take((int)config('diary.display_ata_time'))
-            ->get();
+        // $diaries = Diary::where('published', true)
+        //     ->where(function($query){
+        //             $query->Where('last_displayed', '<=', Carbon::now()
+        //             ->addSecond(-config('diary.interval')))->orWhereNull('last_displayed');
+        //     })
+        //     ->inRandomOrder()
+        //     ->take((int)config('diary.display_ata_time'))
+        //     ->get();
+        $diaries = [];
 
         // recommended diaries for the logged-in user according to forgetting curve
         $fc_dates = [1, 4, 7, 11, 15, 20, 30];
@@ -36,9 +38,30 @@ class DefaultController extends Controller
             $diary->save();
         }
 
+
+        //////// book
+        $recomendations_book = [];
+        $readings = [];
+        if($request->user()){
+            $readings = DB::table('books as b')->select(['b.title as book', 'r.id as id', 'r.title as title', 'r.contents as contents', 'r.updated_at'])->join('readings as r', 'b.id', '=', 'r.book_id')->where('b.user_id', $request->user()->id)->get();
+            $recomendations_book = $readings->filter(function($val){
+                $fc_dates = [1, 4, 7, 11, 15, 20, 30];
+                $d = new Carbon($val->updated_at);
+                $d->setTime(0,0,0);
+                foreach ($fc_dates as $fc_date) {
+                    if($d->eq(Carbon::today()->addDay(-$fc_date))){
+                        return true;
+                    }
+                }
+                return false;
+            });
+        }
+
+
         return view('top.top', [
             "diaries" => $diaries,
             "recomendations" => $recomendations,
+            "books" => $recomendations_book,
         ]);
     }
 }
--- resources/views/top/diaryList.blade.php
+++ resources/views/top/diaryList.blade.php
@@ -12,6 +12,21 @@
         @endforeach
     </div>
     @endif
+
+    @if(count($books)>0)
+    <p class="diarylist-recommendtext"><i class="fas fa-undo-alt"></i> 今日はこの感想を振り返ってみましょう</p>
+    <div class="diarylist-body">
+        @foreach ($books as $diary)
+        <article class="diarylist-item">
+            <a href="{{ route('book.reading.show', ['reading' => $diary->id]) }}"></a>
+            <h2 class="diarylist-item-title">{!! e($diary->title) !!}</h2>
+            <p class="diarylist-item-date">{{ $diary->updated_at }}</p>
+            <p class="diarylist-item-contents">{!! nl2br(e($diary->contents)) !!}</p>
+        </article>
+        @endforeach
+    </div>
+    @endif
+
     <div class="diarylist-body">
         @if(count($diaries)<=0 && count($recomendations)<=0)
         <div class="diarylist-null">
